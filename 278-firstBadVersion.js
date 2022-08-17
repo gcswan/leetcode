a915a963ccd6c2cc50aa91463a8f1d71e7594d2e
Greg Swan <greg.c.swan@gmail.com>
2022-08-16 19:41:33 -0500

    firstBadVersion.js

---
+++ 278-firstBadVersion.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+const isBadVersion = v => v >= 4 ? true : false;
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+ var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 0; 
+        let end = n;
+        while (start <= end) {
+             
+
+
+
+        }
+        
+    };
+};
+
+console.log(isBadVersion(5))
