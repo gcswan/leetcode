b0df7a2109d7b386261fa608d33cf8926909c37b
Greg Swan <greg.c.swan@gmail.com>
2022-11-14 21:21:16 -0600

    combinationSum without shell

--- 0039-combinationSum.js
+++ 0039-combinationSum.js
@@ -14,12 +14,10 @@
  */
 var combinationSum = function(candidates, target) {
   let result = []
-
   const combine = (start, combo) => {
     let total = combo.reduce((acc,cur) => acc + cur, 0)
     if (total == target) result.push(combo.slice()) 
     if (total >= target) return
-    
     for (let i = start; i < candidates.length; i++) {
       combo.push(candidates[i])
       combine(i, combo)
@@ -27,6 +25,23 @@ var combinationSum = function(candidates, target) {
     }
   }
   combine(0, [])
+  return result
+};
+
+/** 
+ * same recursive function without the shell. The first version seems to be 
+ * faster on leetcode. I believe it's because I'm not passing in candidates, 
+ * target, and result on every invocation. 
+ */
+var combinationSum = function(candidates, target, start = 0, combo = [], result = []) {
+  let total = combo.reduce((acc,cur) => acc + cur, 0)
+  if (total == target) result.push(combo.slice()) 
+  if (total >= target) return result
+  for (let i = start; i < candidates.length; i++) {
+    combo.push(candidates[i])
+    combinationSum(candidates, target, i, combo, result)
+    combo.pop()
+  }
   return result
 };
 
