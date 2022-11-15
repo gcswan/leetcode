f2d39055ab7f93bb2298afa8aa0fc9d834659b25
Greg Swan <greg.c.swan@gmail.com>
2022-11-14 21:04:33 -0600

    combinationSum

---
+++ 0039-combinationSum.js
@@ -0,0 +1,33 @@
+/**
+ * @description Given an array of distinct integers candidates and a target 
+ * integer target, return a list of all unique combinations of candidates where
+ * the chosen numbers sum to target. You may return the combinations in any order.
+ * The same number may be chosen from candidates an unlimited number of times. 
+ * Two combinations are unique if the frequency of at least one of the chosen 
+ * numbers is different.
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ * @example 
+ * // returns [[2,2,3],[7]]
+ * combinationSum([2,3,6,7],7)
+ */
+var combinationSum = function(candidates, target) {
+  let result = []
+
+  const combine = (start, combo) => {
+    let total = combo.reduce((acc,cur) => acc + cur, 0)
+    if (total == target) result.push(combo.slice()) 
+    if (total >= target) return
+    
+    for (let i = start; i < candidates.length; i++) {
+      combo.push(candidates[i])
+      combine(i, combo)
+      combo.pop()
+    }
+  }
+  combine(0, [])
+  return result
+};
+
+console.log(combinationSum([2,3,6,7], 7))
