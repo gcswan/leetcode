269491e27e7015e8540b22dd47d29f15d4f9fdc0
Greg Swan <greg.c.swan@gmail.com>
2022-11-14 19:57:35 -0600

    subsets II

--- 0090-subsetsII.js
+++ 0090-subsetsII.js
@@ -1,18 +1,26 @@
+/**
+ * @description Given an integer array nums that may contain duplicates, return all possible
+ * subsets (the power set)
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
+ * @param {number[]} nums
+ * @return {number[][]}
+ * @example 
+ * // returns [[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * subsetsWithDup([1,2,2])
+ */
 var subsets = function(nums) {
-  result = []; // no need this if function is executed once
+  result = []; 
+  var backtrack = function(start, nums, subset){
+    result.push(subset.slice(0));
+    for(let i = start; i < nums.length;i++){
+      if (i > start && nums[i] === nums[i-1]) continue
+      subset.push(nums[i]);
+      backtrack(i+1, nums,subset);
+      subset.pop();
+    }
+  }
   backtrack(0,nums,[]);
   return result;
 };
+console.log(subsets([1,2,2])) 
 
-var backtrack = function(start, nums, subset){
-  result.push(subset.slice(0));
-
-  for(let i = start; i < nums.length;i++){
-    if (i > start && nums[i] === nums[i-1]) continue
-    subset.push(nums[i]);
-    backtrack(i+1, nums,subset);
-    subset.pop();
-  }
-}
-console.log('[[],[1],[1,2],[1,2,2],[2],[2,2]]', '\n')
-console.log(JSON.stringify(subsets([1,2,2]))) //=> [[],[1],[1,2],[1,2,2],[2],[2,2]]
